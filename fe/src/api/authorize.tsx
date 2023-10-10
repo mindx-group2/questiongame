b39b93c632342e27355702da9395106e8697a099
Tien <tienlehung1080@gmail.com>
2023-10-10 10:45:29 +0700

    fixed error 404

--- fe/src/api/authorize.tsx
+++ fe/src/api/authorize.tsx
@@ -2,7 +2,7 @@ import { axiosClient } from "./axiosConnect";
 import { User } from "../interface/auth";
 
 async function getLogin(data: User) {
-  return await axiosClient.get("/auth/login", data);
+  return await axiosClient.post("/auth/login", data);
 }
 
 async function postRegister(data: User) {
