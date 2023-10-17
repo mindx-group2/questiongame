69aed1b6b0dea38490cb18f72b0b1d41ac3aabbf
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2023-10-18 00:39:57 +0700

    add manage_question

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './pages/login/login';
 import Register from './pages/register/register';
 import JoinGame from './pages/join_game/join_game';
+import ManageQuestion from './pages/manage_question/manage_question';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<Login />} />
           <Route path='register' element={<Register />} />
           <Route path='join-game' element={<JoinGame />} />
+          <Route path='manage-question' element={<ManageQuestion />} />
         </Route>
       </Routes>
     </>
