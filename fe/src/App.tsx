be1490cc76f48a60f1b527bf1d76c0fe4a72a8f9
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2023-10-17 15:49:09 +0700

    add join_game

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from './pages/login/login';
 import Register from './pages/register/register';
+import JoinGame from './pages/join_game/join_game';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path='/'>
           <Route index element={<Login />} />
           <Route path='register' element={<Register />} />
+          <Route path='join-game' element={<JoinGame />} />
         </Route>
       </Routes>
     </>
